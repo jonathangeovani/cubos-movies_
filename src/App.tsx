6c36ec81d645a82f3f402e51bd1600ae7ea63a12
jonathangeovani <dongeovanimenor@gmail.com>
2025-08-09 16:56:09 -0300

    add tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,17 @@
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen";
 import "./App.css";
 
+const router = createRouter({ routeTree });
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: typeof router;
+  }
+}
+
 function App() {
-  return (
-    <>
-      <div>Hello, world!</div>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
