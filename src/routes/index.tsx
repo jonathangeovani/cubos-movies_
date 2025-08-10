912ce36790c0548e1d2e5b99e141fcbb8d14ad2d
jonathangeovani <dongeovanimenor@gmail.com>
2025-08-10 13:03:28 -0300

    add SearchBarContainer component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
+import SearchBarContainer from "../components/SearchBarContainer";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
-  return <div>Hello "/"!</div>;
+  return (
+    <>
+      <SearchBarContainer />
+    </>
+  );
 }
