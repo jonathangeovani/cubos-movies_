1b06f7c9de89dba6b64335b8d5c3ac55fdfd3831
jonathangeovani <dongeovanimenor@gmail.com>
2025-08-10 22:47:18 -0300

    add MoviesContainer component

--- src/components/MoviesContainer/index.tsx
+++ src/components/MoviesContainer/index.tsx
@@ -1,7 +1,62 @@
+import MovieCard from "../MovieCard";
 import "./style.scss";
 
+const data = [
+  {
+    title: "Movie 1",
+    categories: ["Ação", "Aventura", "Ficção Científica"],
+    img: "https://placehold.co/235x355",
+    score: 50,
+    onClick: () => console.log("Movie 1"),
+  },
+  {
+    title: "Movie 2",
+    categories: ["Ação", "Aventura", "Drama"],
+    img: "https://placehold.co/235x355",
+    score: 60,
+    onClick: () => console.log("Movie 2"),
+  },
+  {
+    title: "Movie 3",
+    categories: ["Ação", "Aventura", "Drama"],
+    img: "https://placehold.co/235x355",
+    score: 70,
+    onClick: () => console.log("Movie 3"),
+  },
+  {
+    title: "Movie 4",
+    categories: ["Ação", "Aventura", "Drama"],
+    img: "https://placehold.co/235x355",
+    score: 80,
+    onClick: () => console.log("Movie 4"),
+  },
+  {
+    title: "Movie 5",
+    categories: ["Ação", "Aventura", "Drama"],
+    img: "https://placehold.co/235x355",
+    score: 100,
+    onClick: () => console.log("Movie 5"),
+  },
+];
+
 const MoviesContainer = () => {
-  return <div className="main-container">Movies Container</div>;
+  return (
+    <div className="main-container">
+      {data &&
+        data.map((movie, index) => {
+          return (
+            <MovieCard
+              key={index}
+              title={movie.title}
+              categories={movie.categories}
+              img={movie.img}
+              percentage={movie.score}
+              onClick={movie.onClick}
+            />
+          );
+        })}
+    </div>
+  );
 };
 
 export default MoviesContainer;
