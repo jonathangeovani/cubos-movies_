490358bb1e4723232402e9939c6d1b76af241689
jonathangeovani <dongeovanimenor@gmail.com>
2025-08-11 22:42:46 -0300

    add PaginationContainer component

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,14 @@
 import { createFileRoute } from "@tanstack/react-router";
 import SearchBarContainer from "../components/SearchBarContainer";
 import MoviesContainer from "../components/MoviesContainer";
+import PaginationContainer from "../components/PaginationContainer";
 
 const Index = () => {
   return (
     <>
       <SearchBarContainer />
       <MoviesContainer />
+      <PaginationContainer currentPage={1} totalPages={5} />
     </>
   );
 };
