e8b2165b765892cb3722571a610671389478fb60
jonathangeovani <dongeovanimenor@gmail.com>
2025-08-11 23:14:53 -0300

    add types

---
+++ src/types/index.ts
@@ -0,0 +1,8 @@
+// export type { AppContextContent } from "./AppContextContent.ts";
+// export type { HomeContextContent } from "./HomeContextContent.ts";
+export type { MovieData } from "./MovieData.ts";
+export type { MovieDetailsData } from "./MovieDetailsData.ts";
+// export type { MovieCreditsData } from "./MovieCreditsData.ts";
+export type { MovieTrailerData } from "./MovieTrailerData.ts";
+// export type { PersonDetailsData } from "./PersonDetailsData.ts";
+// export type { PersonMoviesData } from "./PersonMoviesData.ts";
